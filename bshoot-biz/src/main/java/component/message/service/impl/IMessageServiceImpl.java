5562c40b59ee6d8b3a4e8758fae8e35c1166add3
zhi.huang <zhi.huang@dianping.com>
2015-12-28 22:42:17 +0800

    消息推送服务

--- bshoot-biz/src/main/java/component/message/service/impl/IMessageServiceImpl.java
+++ bshoot-biz/src/main/java/component/message/service/impl/IMessageServiceImpl.java
@@ -1,12 +1,17 @@
 package component.message.service.impl;
 
 import component.message.service.IMessageService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by john on 15/12/28.
  */
+@Service
 public class IMessageServiceImpl implements IMessageService {
     public boolean sendMessage(String userId, String jsonText) {
-        return false;
+        //TODO redis里获取用户所在ip
+        String messageServerIp = "127.0.0.1";
+        //TODO 发送httpPost数据
+        return true;
     }
 }
