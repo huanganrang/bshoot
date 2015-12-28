af2fa29fbe91adc1d33cc2620268b54f5fe9f677
zhi.huang <zhi.huang@dianping.com>
2015-12-28 22:35:40 +0800

    推送服务

--- bshoot-biz/src/main/java/component/message/service/IMessageService.java
+++ bshoot-biz/src/main/java/component/message/service/IMessageService.java
@@ -4,7 +4,7 @@ package component.message.service;
  * 消息推送服务
  * Created by john on 15/12/28.
  */
-public interface IMessage {
+public interface IMessageService {
 
-    boolean sendMessage(String userId, String jsontext);
+    boolean sendMessage(String userId, String jsonText);
 }
