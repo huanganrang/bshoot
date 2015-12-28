3fa9d18a91faea4a8aef1b623d7f6722ea30c036
zhi.huang <zhi.huang@dianping.com>
2015-12-28 22:36:55 +0800

    推送服务

---
+++ bshoot-biz/src/main/java/component/message/service/impl/IMessageServiceImpl.java
@@ -0,0 +1,12 @@
+package component.message.service.impl;
+
+import component.message.service.IMessageService;
+
+/**
+ * Created by john on 15/12/28.
+ */
+public class IMessageServiceImpl implements IMessageService {
+    public boolean sendMessage(String userId, String jsonText) {
+        return false;
+    }
+}
