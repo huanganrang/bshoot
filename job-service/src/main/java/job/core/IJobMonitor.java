1e12addf8136933ef6856a4dbb15dca275735c93
zhouyibing <zhouyibing_1990@163.com>
2016-01-22 00:19:02 +0800

    job服务

---
+++ job-service/src/main/java/job/core/IJobMonitor.java
@@ -0,0 +1,8 @@
+package job.core;
+
+/**
+ * Created by zhou on 2016/1/21.
+ * job监控，监控job的运行状态，进度，异常等
+ */
+public interface IJobMonitor {
+}
---
+++ job-service/src/main/java/job/task/ITask.java
@@ -0,0 +1,9 @@
+package job.task;
+
+/**
+ * Created by zhou on 2016/1/21.
+ * job任务顶级类
+ */
+public interface ITask {
+    void execute();
+}
